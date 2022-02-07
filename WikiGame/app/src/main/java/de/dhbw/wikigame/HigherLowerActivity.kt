2d0e39b47d37558466e4ab62a405bcb8a5c85d46
Theis <ma.th@web.de>
2022-02-07 12:00:23 +0100

    add empty activities

---
+++ WikiGame/app/src/main/java/de/dhbw/wikigame/HigherLowerActivity.kt
@@ -0,0 +1,11 @@
+package de.dhbw.wikigame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HigherLowerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_higher_lower)
+    }
+}
