ffda65dbd45e842318039567d177804c6deb7ad6
Theresa Knapp <Theresa.Knapp@outlook.de>
2022-02-07 14:53:59 +0100

    Added and inflated XML - Part I

--- WikiGame/app/src/main/java/de/dhbw/wikigame/GameOverActivity.kt
+++ WikiGame/app/src/main/java/de/dhbw/wikigame/GameOverActivity.kt
@@ -2,11 +2,14 @@ package de.dhbw.wikigame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import de.dhbw.wikigame.databinding.ActivityGameOverBinding
+
+private lateinit var binding: ActivityGameOverBinding
 
 class GameOverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_over)
-        //Test GameOverBranch
+        binding = ActivityGameOverBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
