9f070f8a1d2068532e6f746b180d30afcabb2e7a
Theis <ma.th@web.de>
2022-02-21 14:06:14 +0100

    add logic for game

--- WikiGame/app/src/main/java/de/dhbw/wikigame/HigherLowerActivity.kt
+++ WikiGame/app/src/main/java/de/dhbw/wikigame/HigherLowerActivity.kt
@@ -14,6 +14,7 @@ import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.squareup.picasso.Picasso
+import de.dhbw.wikigame.api.wikimedia.datatypes.WikimediaArticleStatistics
 import de.dhbw.wikigame.api.wikimedia.interfaces.WikimediaStatsInterface
 import de.dhbw.wikigame.api.wikipedia.handlers.images.ArticleThumbnailAPIHandler
 import de.dhbw.wikigame.databinding.ActivityHigherLowerBinding
@@ -21,79 +22,92 @@ import java.util.*
 import kotlin.concurrent.schedule
 
 private lateinit var binding: ActivityHigherLowerBinding
+private lateinit var viewModel: HigherLowerActivityViewModel
+private lateinit var article1: WikimediaArticleStatistics
+private lateinit var article2: WikimediaArticleStatistics
+private lateinit var wikimediaStatsInterface: WikimediaStatsInterface
 
 class HigherLowerActivity : AppCompatActivity() {
     var score = 0
-
-    lateinit var viewModel: HigherLowerActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHigherLowerBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val mostViewedArticlesJSONString: String = intent.getStringExtra("mostViewedArticlesJSONString")!!
-
-        val wikimediaStatsInterface: WikimediaStatsInterface = WikimediaStatsInterface(mostViewedArticlesJSONString, "{}")
-
-        var article1 = wikimediaStatsInterface.getRandomWikiArticle()
-        var article2 = wikimediaStatsInterface.getRandomWikiArticle()
-
         viewModel = ViewModelProvider(this).get(HigherLowerActivityViewModel::class.java)
 
-        viewModel.currentFirstArticleThumbnailURL.observe(this, Observer{
+        //get data from wikipedia api
+        val mostViewedArticlesJSONString: String = intent.getStringExtra("mostViewedArticlesJSONString")!!
+        wikimediaStatsInterface = WikimediaStatsInterface(mostViewedArticlesJSONString, "{}")
 
-            println("observe thumb1: ${viewModel.currentFirstArticleThumbnailURL.value}")
-            Picasso.get()
-                .load(viewModel.currentFirstArticleThumbnailURL.value)
-                .into(binding.thumbnail1)
-            binding.lable1.text = article1!!.article
-            binding.viewCount1.text = article1!!.views.toString()
-
-        })
-
-        viewModel.currentSecondArticleThumbnailURL.observe(this, Observer{
-
-            println("observe thumb2: ${viewModel.currentSecondArticleThumbnailURL.value}")
-            Picasso.get()
-                .load(viewModel.currentSecondArticleThumbnailURL.value)
-                .into(binding.thumbnail2)
-            binding.lable2.text = article2!!.article
-        })
-
-
-        val articleThumbnailAPIHandler: ArticleThumbnailAPIHandler = ArticleThumbnailAPIHandler()
-        articleThumbnailAPIHandler.getWikipediaArticleThumbnailURL(article1!!.article, viewModel.currentFirstArticleThumbnailURL)
-        articleThumbnailAPIHandler.getWikipediaArticleThumbnailURL(article2!!.article, viewModel.currentSecondArticleThumbnailURL)
+        //initialize game with first two articles
+        article1 = (wikimediaStatsInterface.getRandomWikiArticle())!!
+        article2 = (wikimediaStatsInterface.getRandomWikiArticle())!!
+        loadArticlesToView(article1, article2)
 
 
 
+        //buttons
         binding.higherButton.setOnClickListener {
             if (article1!!.views < article2!!.views) {
                 increaseScore()
+                displayCheckmark()
+                showNewArticles()
             } else {
                 gameOver(score)
             }
         }
-
         binding.lowerButton.setOnClickListener {
             if (article1!!.views > article2!!.views) {
                 increaseScore()
+                displayCheckmark()
+                showNewArticles()
             } else {
                 gameOver(score)
             }
         }
     }
 
+    //game logic
     fun increaseScore() {
         score++
         binding.scoreValue.text = score.toString()
+    }
 
+    fun displayCheckmark(){
         //display checkmark for 0.5s
         binding.checkmark.visibility = View.VISIBLE
         Timer("CheckMarkTimer", false).schedule(500) {
             binding.checkmark.visibility = View.INVISIBLE
         }
+    }
+
+    fun showNewArticles(){
+        article1 = article2
+        article2 = (wikimediaStatsInterface.getRandomWikiArticle())!!
+        loadArticlesToView(article1, article2)
+    }
+
+    fun loadArticlesToView(article1: WikimediaArticleStatistics?, article2: WikimediaArticleStatistics?){
+        viewModel.currentFirstArticleThumbnailURL.observe(this, Observer{
+            println("observe thumb1: ${viewModel.currentFirstArticleThumbnailURL.value}")
+            Picasso.get()
+                .load(viewModel.currentFirstArticleThumbnailURL.value)
+                .into(binding.thumbnail1)
+            binding.lable1.text = article1!!.article
+            binding.viewCount1.text = article1!!.views.toString()
+        })
+        viewModel.currentSecondArticleThumbnailURL.observe(this, Observer{
+            println("observe thumb2: ${viewModel.currentSecondArticleThumbnailURL.value}")
+            Picasso.get()
+                .load(viewModel.currentSecondArticleThumbnailURL.value)
+                .into(binding.thumbnail2)
+            binding.lable2.text = article2!!.article
+        })
+        //handle loading thumbnails
+        val articleThumbnailAPIHandler: ArticleThumbnailAPIHandler = ArticleThumbnailAPIHandler()
+        articleThumbnailAPIHandler.getWikipediaArticleThumbnailURL(article1!!.article, viewModel.currentFirstArticleThumbnailURL)
+        articleThumbnailAPIHandler.getWikipediaArticleThumbnailURL(article2!!.article, viewModel.currentSecondArticleThumbnailURL)
     }
 
     fun gameOver(score: Int){
@@ -104,7 +118,7 @@ class HigherLowerActivity : AppCompatActivity() {
         finish()
     }
 
-    //Menu stuff
+    //Menu bar
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
