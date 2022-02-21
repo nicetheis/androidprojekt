cc26869039d613927473be36f48283eeecfb5f90
Theis <ma.th@web.de>
2022-02-21 12:59:14 +0100

    improve bindings

--- WikiGame/app/src/main/java/de/dhbw/wikigame/HigherLowerActivity.kt
+++ WikiGame/app/src/main/java/de/dhbw/wikigame/HigherLowerActivity.kt
@@ -16,8 +16,11 @@ import androidx.lifecycle.ViewModelProvider
 import com.squareup.picasso.Picasso
 import de.dhbw.wikigame.api.wikimedia.interfaces.WikimediaStatsInterface
 import de.dhbw.wikigame.api.wikipedia.handlers.images.ArticleThumbnailAPIHandler
+import de.dhbw.wikigame.databinding.ActivityHigherLowerBinding
 import java.util.*
 import kotlin.concurrent.schedule
+
+private lateinit var binding: ActivityHigherLowerBinding
 
 class HigherLowerActivity : AppCompatActivity() {
     var score = 0
@@ -26,7 +29,8 @@ class HigherLowerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_higher_lower)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val mostViewedArticlesJSONString: String = intent.getStringExtra("mostViewedArticlesJSONString")!!
 
@@ -35,16 +39,6 @@ class HigherLowerActivity : AppCompatActivity() {
         var article1 = wikimediaStatsInterface.getRandomWikiArticle()
         var article2 = wikimediaStatsInterface.getRandomWikiArticle()
 
-        val scoreView = findViewById<TextView>(R.id.scoreValue)
-        val higherBtn = findViewById<Button>(R.id.higherButton)
-        val lowerBtn = findViewById<Button>(R.id.lowerButton)
-        val thumb1 = findViewById<ImageView>(R.id.thumbnail1)
-        val thumb2 = findViewById<ImageView>(R.id.thumbnail2)
-        val lable1 = findViewById<TextView>(R.id.lable1)
-        val lable2 = findViewById<TextView>(R.id.lable2)
-        val checkmark = findViewById<ImageView>(R.id.checkmark)
-        val viewCount = findViewById<TextView>(R.id.viewCount1)
-
         viewModel = ViewModelProvider(this).get(HigherLowerActivityViewModel::class.java)
 
         viewModel.currentFirstArticleThumbnailURL.observe(this, Observer{
@@ -52,9 +46,9 @@ class HigherLowerActivity : AppCompatActivity() {
             println("observe thumb1: ${viewModel.currentFirstArticleThumbnailURL.value}")
             Picasso.get()
                 .load(viewModel.currentFirstArticleThumbnailURL.value)
-                .into(thumb1)
-            lable1.text = article1!!.article
-            viewCount.text = article1!!.views.toString()
+                .into(binding.thumbnail1)
+            binding.lable1.text = article1!!.article
+            binding.viewCount1.text = article1!!.views.toString()
 
         })
 
@@ -63,8 +57,8 @@ class HigherLowerActivity : AppCompatActivity() {
             println("observe thumb2: ${viewModel.currentSecondArticleThumbnailURL.value}")
             Picasso.get()
                 .load(viewModel.currentSecondArticleThumbnailURL.value)
-                .into(thumb2)
-            lable2.text = article2!!.article
+                .into(binding.thumbnail2)
+            binding.lable2.text = article2!!.article
         })
 
 
@@ -74,30 +68,31 @@ class HigherLowerActivity : AppCompatActivity() {
 
 
 
-        higherBtn.setOnClickListener {
+        binding.higherButton.setOnClickListener {
             if (article1!!.views < article2!!.views) {
-                increaseScore(scoreView, checkmark)
+                increaseScore()
             } else {
                 gameOver(score)
             }
         }
 
-        lowerBtn.setOnClickListener {
+        binding.lowerButton.setOnClickListener {
             if (article1!!.views > article2!!.views) {
-                increaseScore(scoreView, checkmark)
+                increaseScore()
             } else {
                 gameOver(score)
             }
         }
     }
 
-    fun increaseScore(scoreView: TextView, checkmark: ImageView) {
+    fun increaseScore() {
         score++
-        scoreView.text = score.toString()
-        checkmark.visibility = View.VISIBLE
+        binding.scoreValue.text = score.toString()
 
+        //display checkmark for 0.5s
+        binding.checkmark.visibility = View.VISIBLE
         Timer("CheckMarkTimer", false).schedule(500) {
-            checkmark.visibility = View.INVISIBLE
+            binding.checkmark.visibility = View.INVISIBLE
         }
     }
 
